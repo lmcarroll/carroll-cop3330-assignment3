7015fec26ed8d58fe37039fb2fcacf0a9a5ed036
lmcarroll <liamcarrollapplicant@gmail.com>
2021-10-11 18:28:05 -0400

    Initial Commit

--- src/main/java/ex45/WordFinder.java
+++ src/main/java/ex45/WordFinder.java
@@ -5,15 +5,43 @@
 
 package ex45;
 
-import java.io.File;
-import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 
 public class WordFinder {
 
+    public static void fileCreator (String str, String file) throws FileNotFoundException {
 
+        PrintWriter out = new PrintWriter(file + ".txt");
+
+        out.println(str);
+        out.close();
+    }
+
+    public static String replaceInString(String str) {
+
+        str = str.replaceAll("tiliz", "s");
+
+        return str;
+    }
+
+    public static void main (String[] args) throws IOException {
+
+        System.out.println("What is the name of the new file (do not include .txt at end)? ");
+        Scanner input = new Scanner(System.in);
+        String file = input.nextLine();
+
+        String content = Files.readString(Path.of("exercise45_input.txt"), StandardCharsets.US_ASCII);
+
+        String revised = replaceInString(content);
+
+        fileCreator(revised, file);
+
+}
 
 }
